715deba0f6793cac1ac1d45a1535dc770b65bbb4
rosstimo <rosstimo@isu.edu>
2021-09-07 08:57:31 -0600

    add simple loop structure examples

--- LoopExamples/LoopExamples.vb
+++ LoopExamples/LoopExamples.vb
@@ -1,7 +1,31 @@
-﻿Module LoopExamples
+﻿Option Explicit On
 
-    Sub Main()
 
-    End Sub
+Module LoopExamples
 
-End Module
+        Sub Main()
+
+        'Runs a number of times increment by Step
+        Dim pattern As String
+        For i = 1 To 10 Step 1
+            'Console.WriteLine(i)
+            pattern &= "*"
+            Console.WriteLine(pattern)
+        Next
+
+        Dim count As Integer '= 11
+        'Evaluate before entering the loop
+        Do While count <= 10
+            count += 1
+        Loop
+
+        'evaluate at the end of the loop
+        'always runs the nested code at least once
+        Do
+            count += 1
+        Loop While count <= 10
+
+        Console.Read()
+        End Sub
+
+    End Module
